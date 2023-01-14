c92482666e3af370c1358834576960169f2eba70
fsasha1973 <r8476@mail.ru>
2023-01-15 00:03:04 +0500

    lr 6

--- src/task_1264/Main.java
+++ src/task_1264/Main.java
@@ -3,13 +3,14 @@ package task_1264;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException
-    {
-        Scanner in = new Scanner(System.in, "ISO-8859-1");
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
+    public static void main(String[] args) throws IOException{
+
+        Scanner in = new Scanner(System.in, StandardCharsets.ISO_8859_1);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.ISO_8859_1));
         int N,M;
         N=in.nextInt();
         M=in.nextInt();
--- src/task_1753/Main.java
+++ src/task_1753/Main.java
@@ -1,12 +1,11 @@
 package task_1753;
 
 import java.util.Scanner;
-
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
-public class Main {
-    public static void main(String[] args) {
+        public class Main {
+            public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
@@ -16,10 +15,11 @@ public class Main {
 
         double x   = sqrt(pow(h, 4.0/3.0) * pow(H, 2.0/3.0)/pow(2, 2.0/3.0) - pow(h, 2.0));
         double res = H/2.0*1/sqrt(1 + pow(h/x, 2.0)) - x;
-        if (res <= L && res > 0)
-        System.out.format("%.6f", res);
-        else
-        System.out.format("%.6f", 0.0);
+        if (res <= L && res > 0) {
+            System.out.format("%.6f", res);
+        } else {
+            System.out.format("%.6f", 0.0);
+        }
 
     }
 }
